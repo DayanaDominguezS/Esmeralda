0de1c61453735e3d7a5bc899b1280eea8e369f98
ddominguezs <dpds2906hotmail.com>
2023-10-04 19:24:56 -0500

    ingreso de carpetas para inicio proyecto Esmeralda fase 1

---
+++ BACKEND/Esmeralda/src/main/java/com/HA/Esmeralda/domain/Empleado.java
@@ -0,0 +1,23 @@
+package com.HA.Esmeralda.domain;
+
+import java.time.LocalDate;
+
+public class Empleado {
+
+    private Long id;
+
+    private String primerNombre;
+
+    private String segundoNombre;
+
+    private String primerApellido;
+
+    private String segundoApellido;
+
+    private LocalDate fechaNacimiento;
+
+    private Long numeroDocIdentidad;
+
+    private String lugarExpDocIdentidad;
+
+}
---
+++ BACKEND/Esmeralda/src/test/java/com/HA/Esmeralda/EsmeraldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HA.Esmeralda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmeraldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
